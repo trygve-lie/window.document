28f545dbcf802f7a00b93da186fb4a58ebe0e3c9
Trygve Lie <github@trygve-lie.com>
2014-06-11 16:39:13 +0200

    Added initial test structure

---
+++ test/document.js
@@ -0,0 +1,8 @@
+/* jshint node: true, strict: true */
+/* global describe: true, it: true, before: true */
+
+"use strict";
+
+var mocha           = require('mocha'),
+    assert          = require('chai').assert,
+    document     	= require('../');
